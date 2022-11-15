f4e5dc1aa6a4d1290aee5266608d308890d23127
sebasnn86 <s.narbay@physaro.com>
2022-11-15 18:23:39 +0100

    Added the useDarkMode hook from usehooks-ts Added some basic styling and functionality, liked it so far. I think as far as demo and cool goes, this is were I'll be leaving.

--- discord-nav-dup/src/App.tsx
+++ discord-nav-dup/src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import SideBar from "./sidebar";
+import { useDarkMode } from "usehooks-ts";
 
 export default function App() {
+	const { isDarkMode } = useDarkMode();
 	return (
-		<div className="bg-gray-200 w-full h-full min-h-screen min-w-[100vw] flex items-center justify-center">
+		<div
+			className={`bg-gray-200 w-full h-full min-h-screen min-w-[100vw] flex items-center justify-center ${
+				isDarkMode ? "dark" : "light"
+			}`}
+		>
 			<SideBar />
 		</div>
 	);
--- discord-nav-dup/src/sidebar.tsx
+++ discord-nav-dup/src/sidebar.tsx
@@ -6,16 +6,29 @@ import {
 	FaAccusoft,
 	FaAcquisitionsIncorporated,
 	FaAd,
+	FaSun,
+	FaMoon,
 } from "react-icons/fa";
+import { useDarkMode } from "usehooks-ts";
 
 type Props = {};
 
 const SideBar = (props: Props) => {
+	const { isDarkMode, toggle } = useDarkMode();
 	return (
 		<div
-			className="fixed top-0 left-0 px-4 w-auto h-screen bg-gray-500 shadow-lg
-                        flex flex-col"
+			className="fixed top-0 left-0 px-4 w-auto h-screen  shadow-lg
+                        flex flex-col
+                        bg-gray-100 dark:bg-gray-900"
 		>
+			<div onClick={toggle}>
+				<SidebarIcon
+					icon={
+						isDarkMode ? <FaSun size={28} /> : <FaMoon size={28} />
+					}
+					text={`Switch to ${isDarkMode ? "Light" : "Dark"} mode`}
+				/>
+			</div>
 			<SidebarIcon icon={<Fa500Px size={28} />} />
 			<SidebarIcon icon={<FaAccessibleIcon size={28} />} />
 			<SidebarIcon icon={<FaAccusoft size={28} />} />
