7016e447959d9317f585aad0bed84c79e06860bb
sebasnn86 <s.narbay@physaro.com>
2022-11-15 18:11:52 +0100

    Ok so this one I didn't knew and I liked. Grouping on a parent to affect it's children, nice one.

--- discord-nav-dup/src/sidebar-icon.tsx
+++ discord-nav-dup/src/sidebar-icon.tsx
@@ -2,10 +2,18 @@ import React from "react";
 
 type Props = {
 	icon: React.ReactNode;
+	text?: string;
 };
 
-const SidebarIcon = ({ icon }: Props) => {
-	return <div className="sidebar-icon">{icon}</div>;
+const SidebarIcon = ({ icon, text = "tooltip" }: Props) => {
+	return (
+		<div className="sidebar-icon group">
+			{icon}
+			<span className="sidebar-tooltip group-hover:scale-100">
+				{text}
+			</span>
+		</div>
+	);
 };
 
 export default SidebarIcon;
